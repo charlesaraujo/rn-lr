66cffb9e718a63dc073cdba77930710a96350f2f
Charles Araújo <charles_araujo@ymail.com>
2021-10-11 17:36:10 -0300

    new: ajust on keyboard open

--- screens/Layout.js
+++ screens/Layout.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 export default function Layout({ children }) {
-  return <View style={styles.container}>{children}</View>;
+  return (
+    <KeyboardAwareScrollView
+      keyboardShouldPersistTaps="always"
+      contentContainerStyle={styles.container}
+    >
+      <View style={styles.container}>{children}</View>
+    </KeyboardAwareScrollView>
+  );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
