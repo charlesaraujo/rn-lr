734e9dc730eb03840b1e92fea539384fe979455a
Charles Araújo <charles_araujo@ymail.com>
2021-10-11 12:18:58 -0300

    refact: add form to home

--- components/Form.js
+++ components/Form.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import tw from "twrnc";
+import FormLabel from "./FormLabel";
 
-const FormInputGroup = [];
+const FormInputGroup = ({ children }) => {
+  return <View style={tw`my-3 space-y-2`}>{children}</View>;
+};
 
 export default function Form() {
-  return <View></View>;
+  return (
+    <View>
+      <FormInputGroup>
+        <FormLabel text="email" />
+      </FormInputGroup>
+    </View>
+  );
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import Title from "../components/Title";
+import Form from "../components/Form";
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Title text="Login" />
+      <Form />
     </View>
   );
 }
